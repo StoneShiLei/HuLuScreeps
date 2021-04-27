592a0975664b2fe9114fb2182558c37f59c986e6
dean <569812422@qq.com>
2021-04-27 18:33:17 +0800

    临时提交

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import { forOwn } from "lodash";
 import { ErrorMapper } from "modules/ErrorMapper";
+import { type } from "os";
 import mountWork from './mount'
-import creepNumberListener from './modules/creepController'
 import { doing } from './utils'
 
 
@@ -10,9 +11,29 @@ export const loop = ErrorMapper.wrapLoop(() => {
   mountWork()
 
   // creep 数量控制
-  creepNumberListener()
+  // creepNumberListener()
 
   // 所有建筑、creep、powerCreep 执行工作
   doing(Game.structures, Game.creeps)
 
+  enum Role{
+    Role1 = "role1",
+    Role2 = "role2"
+  }
+  class WorkFunc{
+    work1:Function
+    work2:Function
+  }
+  let x:{[key in Role]:WorkFunc} = {
+    role1:{
+      work1:console.log,
+      work2:console.log,
+    },
+    role2:{
+      work1:console.log,
+      work2:console.log
+    }
+  }
+
+  x[Role.Role1].work1(1);
 });
