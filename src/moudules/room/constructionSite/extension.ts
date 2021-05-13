f5be86fd45fdbf37a0120c6e1a0e0c3a5ee0431c
dean <569812422@qq.com>
2021-05-13 18:29:20 +0800

    1

--- src/moudules/room/constructionSite/extension.ts
+++ src/moudules/room/constructionSite/extension.ts
@@ -5,15 +5,17 @@ import BuildTask from "../taskController/task/wokerTask/buildTask";
 export default class ContructionSiteExtension extends ConstructionSite {
 
     onWork():void{
-        if(!this.isActive){
-            let priority = 0
-            const BUILD_PRIORITY_REVERSE = BUILD_PRIORITY.reverse()
-            for (const type of BUILD_PRIORITY_REVERSE) {
-                if(this.structureType !== type) continue
-                priority = BUILD_PRIORITY_REVERSE.indexOf(this.structureType)
-                if(priority < 0) priority = 0
-            }
-            this.room?.workController.addTask(new BuildTask(this.id,priority))
-        }
+        // if(!this.isActive){
+        //     let priority = 0
+        //     const BUILD_PRIORITY_REVERSE = BUILD_PRIORITY.reverse()
+        //     for (const type of BUILD_PRIORITY_REVERSE) {
+        //         if(this.structureType !== type) continue
+        //         priority = BUILD_PRIORITY_REVERSE.indexOf(this.structureType)
+        //         if(priority < 0) priority = 0
+        //     }
+        //     this.room?.workController.addTask(new BuildTask(this.id,priority))
+        // }
+
+        console.log(this.room?.workController.removeTask(new BuildTask(this.id).id))
     }
 }
