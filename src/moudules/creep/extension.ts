02430d9c993c002ee66355387a13547dea76f9a8
StoneShiLei <569812422@qq.com>
2021-05-16 23:32:48 +0800

    修复遗迹bug

--- src/moudules/creep/extension.ts
+++ src/moudules/creep/extension.ts
@@ -90,11 +90,11 @@ export default class CreepExtension extends Creep {
     public getEngryFrom(target: AllEnergySource): ScreepsReturnCode {
         let result: ScreepsReturnCode
         // 是建筑 或者遗迹 就用 withdraw
-        if (target instanceof Structure || target instanceof Ruin) {
+        if (target instanceof Ruin || target instanceof Structure) {
             // 如果建筑里没能量了就不去了，防止出现粘性
             if ((target instanceof Structure || target instanceof Ruin ) && target.store[RESOURCE_ENERGY] <= 0) return ERR_NOT_ENOUGH_ENERGY
-            else if(target instanceof Structure)result = this.withdraw(target as Structure, RESOURCE_ENERGY)
             else if(target instanceof Ruin)result = this.withdraw(target as Ruin, RESOURCE_ENERGY)
+            else if(target instanceof Structure)result = this.withdraw(target as Structure, RESOURCE_ENERGY)
             else return ERR_INVALID_TARGET
         }
         else if (target instanceof Resource) result = this.pickup(target as Resource)
